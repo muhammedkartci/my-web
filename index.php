955b95855bc865091ecaf1b49d4a7fd3e3d5e397
muhammedkartci <muhammed.kartci@gmail.com>
2024-06-21 13:46:38 +0300

    Site tamamen değiştirildi

--- index.php
+++ index.php
@@ -10,7 +10,151 @@
 
 <body>
     <div class="header">
+        <div class="logo">
+            <img src="images/muhammed-kartci-logo.png" alt="">
+        </div>
+        <div class="menu">
+            <a href="index.html">Ana Sayfa</a>
+            <a href="#events">Katıldığım Etkinlikler</a>
+            <a href="#hakkimda">Hakkımda</a>
+            <a href="iletisim.html">İletişim</a>
+            <a href="sss.html">SSS</a>
+        </div>
+        <div class="social-media">
+            <a class="logo" href="https://www.instagram.com/kartcimuhammed/?utm_source=ig_web_button_share_sheet"
+                target="_blank"><img src="images/instagram-logo.png" alt=""></a>
+            <a class="logo" href="https://www.facebook.com/" target="_blank"><img src="images/facebook-logo.png"
+                    alt=""></a>
+            <a class="logo" href="https://www.x.com/" target="_blank"><img src="images/twitter-logo.png" alt=""></a>
 
+        </div>
+    </div>
+    <div class="home-banner">
+        <div class="texts">
+            <h2>Başarı, hayal edebilenlerin cesaretindedir.</h2>
+            <p>Hayallerin peşinden koşan gençlerin dünyasına hoş geldiniz. İnanmak, başarmanın yarısıdır. Küçük adımlar,
+                büyük başarıların başlangıcıdır. Şimdi harekete geç, hayal et, inan ve başar!
+            </p>
+            <div class="buttons">
+                <a href="hakkimda.html">Hakkımzda</a>
+                <a href="hakkimda.html">İletişim</a>
+            </div>
+        </div>
+        <div class="home-banner-image">
+            <img src="images/Muhammed Kartcı - HomeBannerImage.png" alt="">
+        </div>
+    </div>
+    <div class="events" id="events">
+        <div class="title">
+            Katıldığım Etkinlikler
+        </div>
+        <div class="description">
+            <p>Katıldığım etkinlikler ve tarihleri.</p>
+        </div>
+        <div class="event-list">
+            <div class="item">
+                <div class="event-logo">
+                    <img src="images/canva-logo.png" alt="">
+                </div>
+                <div class="event-name">
+                    Canva Eğitim
+                </div>
+                <div class="event-date">
+                    26 Temmuz 2021
+                </div>
+            </div>
+            <div class="item">
+                <div class="event-logo">
+                    <img src="images/php-logo.png" alt="">
+                </div>
+                <div class="event-name">
+                    PHP Eğitim
+                </div>
+                <div class="event-date">
+                    07 Temmuz 2022
+                </div>
+            </div>
+            <div class="item">
+                <div class="event-logo">
+                    <img src="images/wordpress-logo.png" alt="">
+                </div>
+                <div class="event-name">
+                    WP Eğitim
+                </div>
+                <div class="event-date">
+                    07 Nisan 2023
+                </div>
+            </div>
+            <div class="item">
+                <div class="event-logo">
+                    <img src="images/premier-pro-logo.png" alt="">
+                </div>
+                <div class="event-name">
+                    Premier Pro
+                </div>
+                <div class="event-date">
+                    07 Temmuz 2023
+                </div>
+            </div>
+            <div class="item">
+                <div class="event-logo">
+                    <img src="images/meta-logo.png" alt="">
+                </div>
+                <div class="event-name">
+                    Sosyal Medya
+                </div>
+                <div class="event-date">
+                    26 Temmuz 2021
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="hakkimda" id="hakkimda">
+        <div class="my-foto">
+            <img src="images/muhammed-kartcı.jpeg" alt="">
+        </div>
+
+        <div class="hakkimda-aciklama">
+            <h1>Hakkımda</h1>
+            <p>Merhaba! Ben Muhammed Fatih Kartcı. 07 Nisan 2008 tarihinde Antalya'da doğdum. Ailemin en küçüğüyüm ve
+                iki abim var. Yazılım ve tasarım alanında çalışıyor, bu alanlardaki bilgi ve becerilerimi sürekli
+                geliştiriyorum. Boş zamanlarımda müzik yapmaktan ve oyun oynamaktan büyük keyif alıyorum. Gelecekte
+                bilgisayar mühendisliği okumak ve bu alanda kariyer yapmak en büyük hayalim. Her gün yeni şeyler
+                öğrenmeye ve kendimi daha da geliştirmeye adadım. İlham veren hikayelerle dolu bir yaşam yolculuğuna
+                çıkmaya hazırım!</p>
+        </div>
+
+    </div>
+    <div class="footer">
+        <div class="footer-menu">
+            <div class="footer-logo">
+                <img src="images/Muhammed Kartcı - Logo Beyaz.png" alt="">
+            </div>
+            <div class="links">
+                <h2>Linkler</h2>
+                <a href="index.html">Ana Sayfa</a>
+                <a href="hakkimizda.html">Hakkımda</a>
+                <a href="iletisim.html">İletişim</a>
+                <a href="sss.html">SSS</a>
+            </div>
+            <div class="links">
+                <h2>Linkler</h2>
+                <a href="index.html">Ana Sayfa</a>
+                <a href="hakkimizda.html">Hakkımda</a>
+                <a href="iletisim.html">İletişim</a>
+                <a href="sss.html">SSS</a>
+            </div>
+            <div class="social">
+                <a class="logo" href="https://www.instagram.com/kartcimuhammed/?utm_source=ig_web_button_share_sheet"
+                    target="_blank"><img src="images/instagram-logo.png" alt=""></a>
+                <a class="logo" href="https://www.facebook.com/" target="_blank"><img src="images/facebook-logo.png"
+                        alt=""></a>
+                <a class="logo" href="https://www.x.com/" target="_blank"><img src="images/twitter-logo.png" alt=""></a>
+            </div>
+        </div>
+        <div class="terms">
+            <p>©2024 Tüm Hakları Saklıdır. Muhammed KARTCI</p>
+        </div>
     </div>
 </body>
 
